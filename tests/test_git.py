d264b10c1d88c734b8346d553377ad281eae09b7
Christian M <chriamue@gmail.com>
2018-12-10 13:58:23 +0100

    temp clone test

--- tests/test_git.py
+++ tests/test_git.py
@@ -2,6 +2,7 @@ import pytest
 import git
 from git import Repo
 import os
+import tempfile
 
 test_repo = os.environ["TEST_REPO"]
 
@@ -13,4 +14,12 @@ def test_clone():
             '.', 'test_repo'), branch='master')
     else:
         repo = Repo('test_repo')
+    print(repo.head.commit)
+
+
+def test_temp_clone():
+    repo = None
+    folder = tempfile.TemporaryDirectory()
+    print(folder.name)
+    repo = git.Repo.clone_from(test_repo, folder.name, branch='master')
     print(repo.head.commit)
