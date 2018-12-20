1320b1d3e557ee193b87c0bef3915dea7a623e9f
Christian M <chriamue@gmail.com>
2018-12-20 13:38:10 +0100

    added cookiecutter

--- mlplatform_cli/domain.py
+++ mlplatform_cli/domain.py
@@ -5,7 +5,7 @@ import tempfile
 import yaml
 from shutil import copyfile
 from .comp import Comp
-
+from cookiecutter.main import cookiecutter
 
 class Domain():
     def __init__(self, url, branch='master'):
@@ -13,29 +13,28 @@ class Domain():
         self.branch = branch
 
     def install(self):
+
         folder = tempfile.TemporaryDirectory()
         repo = git.Repo.clone_from(self.url, folder.name, branch=self.branch)
-        domain_name = None
-        with open(os.path.join(folder.name, 'mlplatform-domain.yml'), 'r') as stream:
+        project_dir = cookiecutter(folder.name)
+        domain_name = os.path.basename(project_dir)
+        with open(os.path.join(project_dir, 'mlplatform-domain.yml'), 'r') as stream:
             domain_cfg = yaml.load(stream)
             domain_name = domain_cfg['name']
             print('[domain] ', domain_name)
-            if not os.path.exists(domain_name):
-                os.makedirs(domain_name)
-            copyfile(os.path.join(folder.name, 'mlplatform-domain.yml'),
-                     os.path.join(domain_name, 'mlplatform-domain.yml'))
-            self.install_docker_compose(domain_name)
+            self.install_docker_compose(project_dir)
             self.install_comps(domaincfg=os.path.join(
-                domain_name, 'mlplatform-domain.yml'), domain_name=domain_name)
+                project_dir, 'mlplatform-domain.yml'), project_dir=project_dir)
+        print(project_dir)
 
-    def install_docker_compose(self, domain_name):
+    def install_docker_compose(self, project_dir):
         dirname = os.path.dirname(__file__)
         docker_compose = os.path.join(dirname, 'res', 'docker-compose.yml')
         copyfile(docker_compose,
-                 os.path.join(domain_name, 'docker-compose.yml'))
+                 os.path.join(project_dir, 'docker-compose.yml'))
 
-    def install_comps(self, domaincfg='mlplatform-domain.yml', domain_name=None):
-        root_path = os.path.join(domain_name, 'comps')
+    def install_comps(self, domaincfg='mlplatform-domain.yml', project_dir=None):
+        root_path = os.path.join(project_dir, 'comps')
         if not os.path.exists(root_path):
             os.makedirs(root_path)
             open(os.path.join(root_path, '__init__.py'), 'a').close()
